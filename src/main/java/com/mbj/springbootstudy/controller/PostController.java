3d20b21481b5b8149ba61c9da391fc80e78da204
meenjoon <zqudwnsz@naver.com>
2023-10-04 14:54:48 +0900

    feat: PostController 구현

---
+++ src/main/java/com/mbj/springbootstudy/controller/PostController.java
@@ -0,0 +1,32 @@
+package com.mbj.springbootstudy.controller;
+
+import com.mbj.springbootstudy.dto.MemberDto;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1/post-api")
+public class PostController {
+
+    @RequestMapping(value = "/domain", method = RequestMethod.POST)
+    public String postExam() {
+        return "Hello Post API";
+    }
+
+    @PostMapping(value = "/member")
+    public String postMember(@RequestBody Map<String, Object> postData) {
+        StringBuilder sb = new StringBuilder();
+
+        postData.entrySet().forEach( map -> {
+            sb.append(map.getKey() + " : " + map.getValue() + "\n");
+        });
+
+        return sb.toString();
+    }
+
+    @PostMapping(value = "/member2")
+    public String postMemberDtp(@RequestBody MemberDto memberDto) {
+        return memberDto.toString();
+    }
+}
